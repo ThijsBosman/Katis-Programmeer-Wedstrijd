f621549c0621ce18b0ecfa0b10fadf58b608a5c9
Thijs <71924766+ThijsBosman@users.noreply.github.com>
2023-01-24 19:03:58 +0100

    Update Opdracht1.py

--- Opdracht1.py
+++ Opdracht1.py
@@ -1,1 +1,12 @@
+n = int(input())
+result = []
 
+for i in range(n):
+	result.append(input())
+
+correct = 0
+
+for i in range(len(result) - 1):
+	correct += result[i] == result[i + 1]
+
+print(correct)
